d64d34320af0b8d3ea699845a1c17ef12375699c
Sungyong Cho <dev@sungyongcho.com>
2023-11-06 22:57:13 +0100

    chore: polish game execution cycle code

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from src.config import *
 class Gomoku:
     def __init__(self, width, height):
         pygame.init()
+        self.width = width
+        self.height = height
+        self.interface = None
         self.game_logic = None
-        self.interface = GameInterface(width, height)
-        self.reset_requested = False
 
-    def main_menu(self):
-        # waiting = True
-        self.interface.new()
+    def init_game(self):
+        self.interface = GameInterface(self.width, self.height)
         self.game_logic = GameLogic()
+        # TODO: load option configuration values and pass to self.game_logic
+        self.interface.new()
         self.interface.set_game_logic(self.game_logic)
 
     def run(self):
@@ -22,10 +24,10 @@ class Gomoku:
             # Check for a reset condition (e.g., a key press 'R')
             if self.interface.reset_requested:
                 self.interface.reset_requested = False  # Reset the flag
-                self.main_menu()  # Go back to the main menu
+                self.init_game()  # Go back to the main menu
 
 
 if __name__ == "__main__":
     game = Gomoku(SCREEN_WIDTH, SCREEN_HEIGHT)
-    game.main_menu()
+    game.init_game()
     game.run()
