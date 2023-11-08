89487b69c590e0afd9dad113fae562e3957950d7
Sungyong Cho <dev@sungyongcho.com>
2023-11-08 12:14:46 +0100

    chore: main menu components changed to pygame_guis

--- src/interface/game_menu.py
+++ src/interface/game_menu.py
@@ -63,37 +63,54 @@ class GameMenu:
         # self.button_options.hide()  # Hide the options button initially
 
     def init_texts(self):
-        font = pygame.font.Font(None, 36)
-        # Create texts for different screens
-        self.text_main = font.render("Main Menu", True, BLACK)
-        self.text_options = font.render("Options Menu", True, BLACK)
-        self.text_name_email_1 = font.render(
-            "by Jung Moo Cheon (cjung-mo@student.42.fr)", True, BLACK
+        # Create UI Label elements for texts on different screens
+        self.text_main = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((self.width / 2, 200), (400, 100)),
+            text="오목 // Gomoku",
+            manager=self.manager,
         )
-        self.text_name_email_2 = font.render(
-            "Sungyong Cho (sucho@student.42.fr)", True, BLACK
+
+        self.text_options = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((700, 200), (200, 50)),
+            text="Options Menu",
+            manager=self.manager,
+            visible=False,
+        )
+
+        self.text_name_email_1 = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((self.width - 550, self.height - 70), (400, 30)),
+            text="by Jung Moo Cheon (cjung-mo@student.42.fr)",
+            manager=self.manager,
+        )
+
+        self.text_name_email_2 = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((self.width - 460, self.height - 40), (300, 30)),
+            text="Sungyong Cho (sucho@student.42.fr)",
+            manager=self.manager,
         )
 
     def draw(self):
         self.screen.fill(WHITE)  # Set a background color for the screen
 
         if self.show_main_menu:
-            self.screen.blit(self.text_main, (700, 200))  # Adjusted text positions
+            self.text_main.show()
+            self.text_options.hide()
             self.button_to_singleplay.show()
             self.button_to_multiplay.show()
             self.button_to_options.show()
             self.button_to_main.hide()
 
         if self.show_options_menu:
-            self.screen.blit(self.text_options, (700, 200))  # Adjusted text positions
+            self.text_main.hide()
+            self.text_options.show()
             self.button_to_singleplay.hide()
             self.button_to_multiplay.hide()
             self.button_to_options.hide()
             self.button_to_main.show()
 
         # TODO: responsive
-        self.screen.blit(self.text_name_email_1, (self.width - 550, self.height - 70))
-        self.screen.blit(self.text_name_email_2, (self.width - 460, self.height - 40))
+        # self.screen.blit(self.text_name_email_1, (self.width - 550, self.height - 70))
+        # self.screen.blit(self.text_name_email_2, (self.width - 460, self.height - 40))
         self.manager.draw_ui(self.screen)
         pygame.display.update()
 
